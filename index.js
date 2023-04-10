e09763ee3daf4ee4e26432eebe89f0f8730c57c9
halfrim0-0 <hitonihiki.develop@gmail.com>
2023-04-10 18:19:58 +0900

    バリデーション追加

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ function calc() {
     let target = document.getElementById("target").value;
     const original = target;
 
+    document.getElementById("error").textContent = isValid(target);
+    if (document.getElementById("error").textContent != "") {
+
+        return;
+    }
+
     let answer = target + " = ";
     let i = 2;
     while (target > 1) {
@@ -28,7 +34,19 @@ function calc() {
     document.getElementById("answer").textContent = answer;
 }
 
+function isValid(target) {
+    if (isNaN(target)) {
+        return "数字を入力してください";
+    } else if (!Number.isInteger(Number(target))) {
+        return "整数を入力してください";
+    } else if (target < 2 || 10000000000 < target) {
+        return "2以上10億以下の数字を入力してください"
+    }
+    return "";
+}
+
 function reset() {
     document.getElementById("answer").textContent = "数字を入力してください";
+    document.getElementById("error").textContent = "";
     document.getElementById("target").value = "";
 }
